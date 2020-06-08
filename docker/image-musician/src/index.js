f4d99dd950078933e64098943073dff6b6fbbbec
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-08 09:05:32 +0200

    Try to figure out UDP broadcasting

--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,11 +1,12 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
-server.setBroadcast(true);
-
 function sendSound() {
   console.log("Sending my sound");
   server.send("salut", port=123, address='255.255.255.255');
 }
 
-setInterval(sendSound, 5 * 1000);
+setInterval(sendSound, 1000);
+server.bind(1234, undefined, function() {
+  //  server.setBroadcast(true);
+});
