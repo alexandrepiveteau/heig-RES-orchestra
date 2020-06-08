f2d4a1f871242ad65f7accd52846e14fc61d3197
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-08 09:54:43 +0200

    Make some cool music

--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -8,10 +8,18 @@ if (process.argv.length != 3) {
 
 const type = process.argv[2];
 
+const map = new Map();
+map.set("piano", "ti-ta-ti");
+map.set("trumpet", "pouet");
+map.set("flute", "trulu");
+map.set("violin", "gzi-gzi");
+map.set("drum", "boum-boum");
+
+const sound = map.get(type);
+
 function sendSound() {
   console.log("Sending my sound");
-  const message = "salut";
-  server.send(message, 0, message.length, 8080, '233.255.255.255');
+  server.send(sound, 0, sound.length, 8080, '233.255.255.255');
 }
 
 setInterval(sendSound, 1000);
