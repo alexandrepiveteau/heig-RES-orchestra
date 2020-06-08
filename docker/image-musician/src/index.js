83368215867e18c12ca02f9347ced89a884c0b46
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-08 09:43:51 +0200

    Retrieve launch arguments

--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,12 +1,17 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
+if (process.argv.length != 3) {
+  console.log('Please provide one argument');
+  process.exit(1);
+}
+
+const type = process.argv[2];
+
 function sendSound() {
   console.log("Sending my sound");
-  server.send("salut", port=123, address='255.255.255.255');
+  const message = "salut";
+  server.send(message, 0, message.length, 8080, '233.255.255.255');
 }
 
 setInterval(sendSound, 1000);
-server.bind(1234, undefined, function() {
-  //  server.setBroadcast(true);
-});
