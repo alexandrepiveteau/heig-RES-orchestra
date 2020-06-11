1307f9a3702ef2a29a77d796bc528a4eb36e7c79
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-11 15:41:53 +0200

    Change musician to use udp multicast

--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,5 +1,5 @@
-const BROADCAST_ADDR="233.255.255.255"
-const PORT=8080
+const MULTICAST_ADDR="239.1.1.100"
+const PORT=9009
 const INTERVAL=1000
 
 const dgram = require('dgram');
@@ -23,7 +23,19 @@ const sound = map.get(type);
 
 function sendSound() {
   console.log("Sending my sound as a " + type);
-  server.send(sound, 0, sound.length, PORT, BROADCAST_ADDR);
+  server.send(sound, 0, sound.length, PORT, MULTICAST_ADDR);
 }
 
-setInterval(sendSound, INTERVAL);
+server.on("error", (err) => {
+    console.log("Got error: ${err.stack}");
+});
+
+server.on("message", (msg, info) => {
+    console.log("Got msg: ${msg}");
+});
+
+server.on("listening", () => {
+    setInterval(sendSound, INTERVAL); // When the socket is ready, send msg
+});
+
+server.bind(PORT);
